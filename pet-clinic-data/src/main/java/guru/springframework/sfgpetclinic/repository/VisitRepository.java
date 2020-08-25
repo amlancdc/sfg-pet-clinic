89e34897c791e34c68923f4e3746d70d64e786b7
Amlan Chowdhury <amlan.cdc@gmail.com>
2020-08-25 17:44:58 +0600

    Implement JPA repository interfaces. Closes #39

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repository/VisitRepository.java
@@ -0,0 +1,7 @@
+package guru.springframework.sfgpetclinic.repository;
+
+import guru.springframework.sfgpetclinic.model.Visit;
+import guru.springframework.sfgpetclinic.services.CrudService;
+
+public interface VisitRepository extends CrudService<Visit, Long> {
+}
